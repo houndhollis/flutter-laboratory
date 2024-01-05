70434e9048618131a979471a5e381a5e3f076cef
houndhollis <houndhollis9797@gmail.com>
2024-01-06 01:49:37 +0900

    싱글톤 테스트 View

--- lib/singleton/home_screen.dart
+++ lib/singleton/home_screen.dart
@@ -8,8 +8,46 @@ class HomeScreenSingleTon extends StatefulWidget {
 }
 
 class _HomeScreenSingleTonState extends State<HomeScreenSingleTon> {
+  TextEditingController teEditingController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SingleTon'),
+        backgroundColor: Colors.grey.shade200,
+        elevation: 1.0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: teEditingController,
+            ),
+            const SizedBox(height: 16.0),
+            Row(
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Save'),
+                  ),
+                ),
+                const SizedBox(width: 12),
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Clear'),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 12),
+            Text('1'),
+          ],
+        ),
+      ),
+    );
   }
 }
