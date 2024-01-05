d55ee329d6a0129e5b3b772ebc0a41e8b091a3d8
houndhollis <houndhollis9797@gmail.com>
2024-01-06 01:42:17 +0900

    싱글톤 초기 설정

---
+++ lib/singleton/home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeScreenSingleTon extends StatefulWidget {
+  const HomeScreenSingleTon({super.key});
+
+  @override
+  State<HomeScreenSingleTon> createState() => _HomeScreenSingleTonState();
+}
+
+class _HomeScreenSingleTonState extends State<HomeScreenSingleTon> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
